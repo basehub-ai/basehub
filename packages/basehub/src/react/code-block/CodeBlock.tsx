8f0ecfde66c915b93b418f7bb5622c5adb680500
Julian Benegas <julianbenegas99@gmail.com>
2023-11-15 11:16:02 -0300

    wip

---
+++ packages/basehub/src/react/code-block/CodeBlock.tsx
@@ -0,0 +1,11 @@
+const CodeBlock = () => {
+  return (
+    <div>
+      <header>header</header>
+      <div>
+        <div>Line numbers</div>
+        <pre>code</pre>
+      </div>
+    </div>
+  );
+};
--- packages/basehub/tsup-client.config.ts
+++ packages/basehub/tsup-client.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig, type Options } from "tsup";
 
-export default defineConfig((options: Options) => {
+export default defineConfig((_options: Options) => {
   return {
-    minify: !options.watch,
+    minify: false,
     dts: true,
     entry: {
       react: "./src/react/index.ts",
+      ["react-code-block"]: "./src/react/code-block/index.ts",
+      ["react-rich-text"]: "./src/react/rich-text/index.ts",
     },
     format: ["cjs"],
   };
