c2207c40aa48ae1c87e73f18e4f1c3997351d248
Julian Benegas <julianbenegas99@gmail.com>
2024-01-17 13:35:29 -0300

    fix regression

--- packages/basehub/src/react/rich-text/index.tsx
+++ packages/basehub/src/react/rich-text/index.tsx
@@ -163,17 +163,17 @@ export type CustomBlocksBase = readonly CustomBlockBase[];
 type HandlerMapping<Blocks extends CustomBlocksBase = readonly any[]> = {
   [K in Blocks[number]["__typename"]]: (
     props: Extract<Blocks[number], { __typename: K }>
-  ) => ReactElement;
+  ) => ReactNode;
 };
 
 type MarkHandlerMapping<Blocks extends CustomBlocksBase = readonly any[]> = {
   [K in Blocks[number]["__typename"] as `${K}${SUFFIX_CUSTOM_BLOCK_MARK}`]: (
-    // we use this hack to add a type ofr each custom component to create separate handlers for each custom component -> magic 🧙
+    // we use this hack to add a type for each custom component to create separate handlers for each custom component -> magic 🧙
     props: Extract<
       Blocks[number],
       { __typename: `${K}${SUFFIX_CUSTOM_BLOCK_MARK}` }
     >
-  ) => ReactElement;
+  ) => ReactNode;
 };
 
 export type RichTextProps<
