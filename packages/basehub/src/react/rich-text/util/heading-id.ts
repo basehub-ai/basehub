8cee68d24fa80f0d752d23b4a9f5f7d661563005
Joaquín R. Montes <joaco.rm512@gmail.com>
2024-04-09 12:59:45 -0300

    [FIX]: Use Github Slugger in RichText headings (#90)
    * use github slugger
    
    * changeset
    
    * lock

--- packages/basehub/src/react/rich-text/util/heading-id.ts
+++ packages/basehub/src/react/rich-text/util/heading-id.ts
@@ -14,15 +14,3 @@ export function extractTextFromNode(node?: Node) {
 
   return textContent;
 }
-
-export function incrementID(id: string) {
-  // duplicates are added "-n" at the end.
-  // if the title already has a "-n" at the end, we'll need to increment it.
-  const matches = id.match(/-(\d+)$/); // Fixed regex to match "-n" format
-  if (matches?.[1] !== undefined) {
-    const number = parseInt(matches[1], 10);
-    return id.replace(/-(\d+)$/, `-${number + 1}`);
-  }
-
-  return `${id}-1`;
-}
