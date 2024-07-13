a4da5874ae3bfe200f80560ca2bb7bd525010eee
Julian Benegas <julianbenegas99@gmail.com>
2024-07-13 13:28:36 -0300

    fix build

--- playground/src/app/layout.tsx
+++ playground/src/app/layout.tsx
@@ -1,6 +1,5 @@
 import "./global.css";
 import type { Metadata } from "next";
-import { siteOrigin } from "../lib/constants";
 
 export const metadata: Metadata = {
   title: "BaseHub SDK Playground",
@@ -15,23 +14,6 @@ export const metadata: Metadata = {
     shortcut: "/next.svg",
   },
   manifest: "/manifest.webmanifest",
-  twitter: {
-    card: "summary_large_image",
-    creator: "@basehub-ai",
-    description: "BaseHub SDK Playground",
-    images: [{ width: 1200, height: 630, url: `${siteOrigin}/og.jpg` }],
-    site: "@basementstudio",
-    title: "BaseHub SDK Playground",
-  },
-  openGraph: {
-    description: "BaseHub SDK Playground",
-    images: [{ width: 1200, height: 630, url: `${siteOrigin}/og.jpg` }],
-    locale: "en-US",
-    siteName: "BaseHub SDK Playground",
-    title: "BaseHub SDK Playground",
-    type: "website",
-    url: siteOrigin,
-  },
 };
 
 export default function RootLayout({
