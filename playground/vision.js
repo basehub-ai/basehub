cf55072f708be4472927b38667fbbaf1833c1855
Julian Benegas <julianbenegas99@gmail.com>
2024-07-13 12:55:22 -0300

    asdf

--- playground/vision.js
+++ playground/vision.js
@@ -1,7 +1,7 @@
 const { readdirSync } = require("fs");
 
 function main() {
-  const root = process.cwd() + "/../..";
+  const root = process.cwd() + "/..";
   // recursively log the full tree
   logTree(root);
 
