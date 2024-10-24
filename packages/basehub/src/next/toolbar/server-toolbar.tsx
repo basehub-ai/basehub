dbfe604734913e5643cb7d9ec126ee2ac53f32de
Julian Benegas <julianbenegas99@gmail.com>
2024-10-24 20:52:21 -0300

    add use cache to server toolbar to fix a weird dynamic io error

--- packages/basehub/src/next/toolbar/server-toolbar.tsx
+++ packages/basehub/src/next/toolbar/server-toolbar.tsx
@@ -22,6 +22,7 @@ type ServerToolbarProps = Parameters<typeof basehub>[0];
 export const ServerToolbar = async ({
   ...basehubProps
 }: ServerToolbarProps) => {
+  "use cache";
   const { isForcedDraft } = getStuffFromEnv(basehubProps);
 
   const enableDraftMode = async ({
