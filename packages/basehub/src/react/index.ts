1e4934d72b7b63ba9b1856683bb8f1e4c9f0ace8
Julian Benegas <julianbenegas99@gmail.com>
2024-12-03 16:53:35 -0300

    revert?

---
+++ packages/basehub/src/react/index.ts
@@ -0,0 +1,7 @@
+export {
+  RichText,
+  createRichTextWithDefaultComponents,
+  type RichTextProps,
+  type CustomBlocksBase,
+  type HandlerProps as RichTextHandlerProps,
+} from "./rich-text/primitive";
