94cd6d6ea1fd1ea8f4d179a4981a7b3f830e57f7
Joaquin Montes <joaco.rm512@gmail.com>
2024-12-03 17:05:17 -0300

    client export

--- packages/basehub/src/react/form/primitive.tsx
+++ packages/basehub/src/react/form/primitive.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
 import type { ReactNode } from "react";
-import { sendEvent, updateEvent } from "../../events";
 
 // this needs to match our BSHBEventSchema scalar type so that it _just works_
 export type Field = {
@@ -70,71 +69,6 @@ export type FormProps = {
   >,
   "action" | "onSubmit" | "children"
 >;
-
-export const unstable_Form = ({
-  schema,
-  components,
-  disableDefaultComponents,
-  children,
-  action,
-  ...rest
-}: FormProps): ReactNode => {
-  const fields = schema as Field[] | undefined;
-
-  return (
-    <form
-      {...rest}
-      onSubmit={(e) => {
-        e.preventDefault();
-        const form = e.target as HTMLFormElement;
-        const formData = new FormData(form);
-        const formattedData: Record<string, unknown> = {};
-
-        fields?.forEach((field) => {
-          const key = field.name;
-          const value = formData.get(key);
-          switch (field.type) {
-            case "checkbox":
-              formattedData[key] = value === "on";
-              break;
-            case "select":
-              formattedData[key] = String(value).split(",");
-            case "radio":
-              formattedData[key] = value;
-              break;
-            case "date":
-            case "datetime":
-              formattedData[key] = new Date(value as string).toISOString();
-              break;
-            case "number":
-              formattedData[key] = Number(value);
-              break;
-            default:
-              formattedData[key] = value;
-              break;
-          }
-        });
-        if (action.type === "update") {
-          updateEvent(action.adminKey as any, action.eventId, formattedData);
-        } else {
-          sendEvent(action.ingestKey as any, formattedData);
-        }
-      }}
-    >
-      {fields?.map((node, index) => {
-        return (
-          <FieldNode
-            field={node}
-            key={index}
-            components={components}
-            disableDefaultComponents={disableDefaultComponents}
-          />
-        );
-      })}
-      {children ?? <button type="submit">Submit</button>}
-    </form>
-  );
-};
 
 const defaultHandlers: Handlers = {
   text: (props) => (
@@ -243,7 +177,7 @@ const defaultHandlers: Handlers = {
   hidden: (props) => <input id={props.id} name={props.name} type="hidden" />,
 };
 
-const FieldNode = ({
+export const FieldNode = ({
   field,
   components,
   disableDefaultComponents,
